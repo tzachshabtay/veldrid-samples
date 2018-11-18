a8f34089f0cb09c162a5e41daad9a114ec6951d7
Tzach Shabtay <tzachshabtay@gmail.com>
2018-11-17 21:39:21 -0500

    Textured quad sample
    Just copied & pasted for textured cube and changed names and vertices.

---
+++ src/TexturedQuad/Desktop/Program.cs
@@ -0,0 +1,14 @@
+﻿using SampleBase;
+
+namespace TexturedQuad
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            VeldridStartupWindow window = new VeldridStartupWindow("Textured Quad");
+            TexturedQuad texturedCube = new TexturedQuad(window);
+            window.Run();
+        }
+    }
+}
